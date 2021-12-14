cc4ad89db4a8a51b8e8a5ef180de5b35f3ef463c
AbhijithSree1 <95019328+AbhijithSree1@users.noreply.github.com>
2021-12-14 19:54:53 +0000

    Update systemidstepramptorque.m

--- systemidstepramptorque.m
+++ systemidstepramptorque.m
@@ -1,4 +1,4 @@
-% system identification code_random
+% system identification step ramp torque output
 clc 
 clear all
 load ramp;
@@ -10,11 +10,6 @@ y2=rampsignal.Y(2).Data';
 dry1 = iddata(y1,u1,1e-3); % sample time 1ms
 dry2 = iddata(y2,u2,1e-3); % sample time 1ms
 ze=merge(dry1(1:15000),dry2(22000:37000)); %selecting the range where the most change is
-%ze = merge(dry(8000:14000),dry(36000:42000)); % to train both the increase and decrease in speed.
-%the best training range is 0 to 15000, it provided good values for the
-%same range and entire range of validation. in this puticular system, i
-%have merged the data where the speed increases and decreases, except for
-%the transfer function everything else looks good. especially ARX.
 figure(1)
 plot(ze)
 hold on
@@ -33,16 +28,16 @@ compare(zv,mi,'b')
 m1 = ssest(ze);
 clf
 compare(zv,m1,'b')
-%%
-m2 = arx(ze,[4,1,1]); % Estimate using ARX: 2 poles, 1 zero and 3 delays
+%% ARX
+m2 = arx(ze,[4,1,1]); 
 clf
 compare(zv,m2,'b')
-%%
-m3 = armax(ze,[4,3,4,0]); % Estimate using ARX: 2 poles, 1 zero and 3 delays
+%% ARMAX
+m3 = armax(ze,[4,3,4,0]); 
 clf
 compare(zv,m3,'b')
-%%
-m4 = tfest(ze,3,2,0.1); % Cont. time transfer function with 2 poles, 1 zero and delay of 0.2
+%% transfer function
+m4 = tfest(ze,3,2,0.1); 
 clf
 compare(zv,m4,'b')
 
